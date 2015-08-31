5dfad5c5e028eac9eb79a8cefc44a3d0265dac71
DimaNikishin <dimanikishin@yandex.ru>
2015-08-31 20:43:31 +0300

    ngroute unit test

--- app/app_test.js
+++ app/app_test.js
@@ -2,11 +2,12 @@
 
 describe('myApp module', function() {
 
-  var scope, indexCntrl;
+  var scope, indexCntrl, route;
   beforeEach(module('myApp'));
-  beforeEach(inject(function($controller, $rootScope){
+  beforeEach(inject(function($controller, $rootScope, $route){
     scope = $rootScope;
     indexCntrl = $controller('indexCntrl', {$scope:scope});
+    route = $route;
   }));
 
   describe('indexCntrl controller', function(){
@@ -18,7 +19,7 @@ describe('myApp module', function() {
 
   });
 
-  describe('view2 hide functions works', function(){
+  describe('indexCntrl hide functions works', function(){
 
     it('should ....', inject(function() {
       expect(scope.isError).toBeUndefined();
@@ -28,6 +29,29 @@ describe('myApp module', function() {
       expect(scope.isError).toBe(false);
       expect(scope.isStart).toBe(false);
       expect(scope.isComplete).toBe(false);
+    }));
+
+  });
+
+  describe('app route functions', function(){
+
+    it('should ....', inject(function() {
+      expect(route.routes['/Home'].controller).toBe('View1Ctrl');
+      expect(route.routes['/Home'].templateUrl).
+        toEqual('view1/view1.html');
+
+      expect(route.routes['/Gallery'].templateUrl).
+        toEqual('view2/view2.html');
+      expect(route.routes['/Gallery'].controller).
+        toEqual('View2Ctrl');
+
+      expect(route.routes['/CodeSandbox'].templateUrl).
+        toEqual('view3/view3.html');
+      expect(route.routes['/CodeSandbox'].controller).
+        toEqual('View3Ctrl');
+
+      // otherwise redirect to
+      expect(route.routes[null].redirectTo).toEqual('/Home')
     }));
 
   });
