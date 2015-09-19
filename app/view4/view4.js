f1ff620f538a0fd72a28e1c3f57fb2c9f330195d
DimaNikishin <dimanikishin@yandex.ru>
2015-09-19 14:40:32 +0300

    changes in json and property show function

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -9,17 +9,52 @@ angular.module('myApp.view4', ['ngRoute', 'ngAnimate'])
   });
 }])
 
-.controller('View4Ctrl', ['$scope',function($scope) {
-  $scope.industrySectors = ['Healthcare Sector','Technology Sector','Basic Materials Sector']
+.factory('HealthCareSector', ['$http', function($http) {
+  return $http.get('http://localhost:8080/app/Healthcare Sector.json')
+    .success(function(data) {
+      return angular.fromJson(data);
+    })
+    .error(function(err) {
+      return err;
+    });
+}])
 
+.controller('View4Ctrl', ['$scope','HealthCareSector',function($scope,HealthCareSector) {
+  $scope.industrySectors = ['Healthcare Sector','Technology Sector','Basic Materials Sector']
+  HealthCareSector.success(function(data){
+    $scope.HealthCareSectorData = data;
+  });
+  //TODO: make more detailed inspection
+  $scope.showEnabledProperty = function(property){
+    if(property === undefined){
+      return false;
+    }
+    else{
+      if(property.length === 0){
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+
+  }
 }])
 
 .directive('product',[function(){
   return {
-    scope: {},
+    scope: {
+      planName: '@name',
+      price: '@',
+      propertyOne: '@',
+      propertyTwo: '@',
+      defaultPropertyOne: '@',
+      defaultPropertyTwo: '@',
+      showPropFunction: '&'
+    },
     templateUrl: 'view4/ProductTemplate/ProductTemplate.html',
     link: function(scope, element, attrs){
-      scope.flawor = attrs.name;
+
     }
   }
 }]);
