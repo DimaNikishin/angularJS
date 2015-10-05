bf043155bb195469ef99d63f51e9e721cb8143d8
DimaNikishin <dimanikishin@yandex.ru>
2015-10-05 22:10:55 +0300

    add and remove sectors functions, code refactor

--- app/tests/view4_test.js
+++ app/tests/view4_test.js
@@ -5,11 +5,19 @@ describe('myApp.view4 module', function() {
   describe('view4 controller', function(){
     var view4Ctrl;
     var scope;
+    var cacheFactory;
 
     beforeEach(module('myApp.view4'));
-    beforeEach(inject(function($controller, $rootScope){
+    beforeEach(inject(function($controller, $rootScope,$cacheFactory,HealthCareSector,additionalSectors){
       scope = $rootScope;
-      view4Ctrl = $controller('View4Ctrl', {$scope:scope});
+      view4Ctrl = $controller('View4Ctrl', {$scope:scope,HealthCareSector:HealthCareSector,additionalSectors:additionalSectors});
+      cacheFactory = $cacheFactory;
+
+      spyOn(additionalSectors, 'get').and.callFake(function() {
+        return {
+          success: function(callback) { callback({TechnologyList: '1',BasicMaterialsList:'2'})}
+        };
+      });
     }));
 
     it('should ....', inject(function() {
@@ -17,21 +25,189 @@ describe('myApp.view4 module', function() {
       expect(view4Ctrl).toBeDefined();
     }));
 
-  });
-
-  describe('view4 selectedNames filter', function(){
-    var view4Ctrl;
-    var scope;
-
-    beforeEach(module('myApp.view4'));
-    beforeEach(inject(function($controller, $rootScope){
-      scope = $rootScope;
-      view4Ctrl = $controller('View4Ctrl', {$scope:scope});
-    }));
-
     it('should return string with sorted by length sectors names', inject(function(selectedNamesFilter) {
       //spec body
       expect(selectedNamesFilter([{name:'name'},{name:'name three'},{name:'name two'}])).toEqual("name; name two; name three; ");
+    }));
+
+    it('controller should save array into cache', inject(function(){
+      //first run of controllerFunction when controller is injected
+      expect(scope.industrySectors).toBe(cacheFactory.get('industrySectors').get('industrySectors'));
+      expect(scope.cache).toBe(cacheFactory.get('industrySectors'));
+      expect(scope.showDetails).toBe(true);
+
+      expect(scope.cache.info()).toEqual({
+        id: 'industrySectors',
+        size: 1
+      });
+      expect(scope.cache.get('industrySectors')).toBe(scope.industrySectors);
+
+      scope.cache.removeAll();
+      expect(scope.cache.info()).toEqual({
+        id: 'industrySectors',
+        size: 0
+      });
+      //test second run of controllerFunction and creating scope.selectedHC within
+      scope.controllerFunction();
+      expect(scope.selectedHC).toBe(true);
+
+    }));
+
+    it('function selectedProducts should return true or false depending on argument length',inject(function(){
+
+      expect(scope.selectedProducts([])).toBe(false);
+      expect(scope.selectedProducts([1,2])).toBe(true);
+
+    }));
+
+    it('function selectedSector should change vars depending on argument',inject(function(){
+      //in test also added call of scope.downloadSectors()
+      scope.selectedSector(scope.industrySectors[0].name);
+      expect(scope.selectedHC).toBe(true);
+      expect(scope.selectedTC).toBe(false);
+      expect(scope.selectedBMC).toBe(false);
+      expect(scope.industrySectors[1].list).toEqual('1');
+      expect(scope.industrySectors[2].list).toEqual('2');
+
+      scope.industrySectors[1].list = ['not empty'];
+      scope.industrySectors[2].list = [];
+      scope.selectedSector(scope.industrySectors[1].name);
+      expect(scope.selectedHC).toBe(false);
+      expect(scope.selectedTC).toBe(true);
+      expect(scope.selectedBMC).toBe(false);
+      expect(scope.industrySectors[1].list).toEqual(['not empty']);
+      expect(scope.industrySectors[2].list).toEqual('2');
+
+      scope.industrySectors[1].list = [];
+      scope.industrySectors[2].list = ['not empty'];
+      scope.selectedSector(scope.industrySectors[2].name);
+      expect(scope.selectedHC).toBe(false);
+      expect(scope.selectedTC).toBe(false);
+      expect(scope.selectedBMC).toBe(true);
+      expect(scope.industrySectors[1].list).toEqual('1');
+      expect(scope.industrySectors[2].list).toEqual(['not empty']);
+    }));
+
+    it('function should call for factory and set to additional sectors lists values from factory depending on its length ', inject(function(){
+//tested call to factory by Jasmine.js spies and fake callback, depending on additional sectors lists length, callback from
+// factory should be saved into additional sectors lists
+
+      scope.downloadSectors();
+      expect(scope.industrySectors[1].list).toEqual('1');
+      expect(scope.industrySectors[2].list).toEqual('2');
+
+      scope.industrySectors[1].list = ['not empty'];
+      scope.industrySectors[2].list = [];
+      scope.downloadSectors();
+      expect(scope.industrySectors[1].list).toEqual(['not empty']);
+      expect(scope.industrySectors[2].list).toEqual('2');
+
+      scope.industrySectors[1].list = [];
+      scope.industrySectors[2].list = ['not empty'];
+      scope.downloadSectors();
+      expect(scope.industrySectors[1].list).toEqual('1');
+      expect(scope.industrySectors[2].list).toEqual(['not empty']);
+
+      scope.industrySectors[1].list = ['not empty'];
+      scope.industrySectors[2].list = ['not empty'];
+      scope.downloadSectors();
+      expect(scope.industrySectors[1].list).toEqual(['not empty']);
+      expect(scope.industrySectors[2].list).toEqual(['not empty']);
+
+    }));
+
+    it('addFunction should search selected sector in industrySectors.list and push in industrySectors.selected',inject(function(){
+      scope.industrySectors = [{name:"Healthcare Sector", selected:[], list: [{name: "Health", sectorType:"HC"}]},{name:"Technology Sector", selected:[], list: [{name: "Tech", sectorType:"TC"}]},{name:"Basic Materials Sector", selected:[], list: [{name: "Basic Materials", sectorType:"BMC"}]}];
+      expect(scope.industrySectors[0].selected.length).toBe(0);
+      expect(scope.industrySectors[1].selected.length).toBe(0);
+      expect(scope.industrySectors[2].selected.length).toBe(0);
+      scope.addFunction("Health")
+      expect(scope.industrySectors[0].selected.length).toBe(1);
+      expect(scope.industrySectors[0].selected).toEqual([{name: "Health", sectorType:"HC", selected: true}]);
+      expect(scope.industrySectors[1].selected.length).toBe(0);
+      expect(scope.industrySectors[2].selected.length).toBe(0);
+      scope.addFunction("Tech")
+      expect(scope.industrySectors[0].selected.length).toBe(1);
+      expect(scope.industrySectors[0].selected).toEqual([{name: "Health", sectorType:"HC", selected: true}]);
+      expect(scope.industrySectors[1].selected.length).toBe(1);
+      expect(scope.industrySectors[1].selected).toEqual([{name: "Tech", sectorType:"TC", selected: true}]);
+      expect(scope.industrySectors[2].selected.length).toBe(0);
+      scope.addFunction("Basic Materials")
+      expect(scope.industrySectors[0].selected.length).toBe(1);
+      expect(scope.industrySectors[0].selected).toEqual([{name: "Health", sectorType:"HC", selected: true}]);
+      expect(scope.industrySectors[1].selected.length).toBe(1);
+      expect(scope.industrySectors[1].selected).toEqual([{name: "Tech", sectorType:"TC", selected: true}]);
+      expect(scope.industrySectors[2].selected.length).toBe(1);
+      expect(scope.industrySectors[2].selected).toEqual([{name: "Basic Materials", sectorType:"BMC", selected: true}]);
+    }));
+
+    it('removeFunction should move element to end remove from industrySectors.selected', inject(function(){
+      scope.industrySectors = [{name:"Healthcare Sector", selected:[{name: "Health", sectorType:"HC"}], list: [{name: "Health", sectorType:"HC"}]},{name:"Technology Sector", selected:[{name: "Tech", sectorType:"TC"}], list: [{name: "Tech", sectorType:"TC"}]},{name:"Basic Materials Sector", selected:[{name: "Basic Materials", sectorType:"BMC"}], list: [{name: "Basic Materials", sectorType:"BMC"}]}];
+      expect(scope.industrySectors[0].selected.length).toBe(1);
+      expect(scope.industrySectors[1].selected.length).toBe(1);
+      expect(scope.industrySectors[2].selected.length).toBe(1);
+
+      scope.removeFunction("Health");
+      expect(scope.industrySectors[0].selected.length).toBe(0);
+      expect(scope.industrySectors[0].list).toEqual([{name: "Health", sectorType:"HC", selected: false}]);
+
+      expect(scope.industrySectors[1].selected.length).toBe(1);
+      expect(scope.industrySectors[1].list).toEqual([{name: "Tech", sectorType:"TC"}]);
+      expect(scope.industrySectors[1].selected).toEqual([{name: "Tech", sectorType:"TC"}]);
+
+      expect(scope.industrySectors[2].selected.length).toBe(1);
+      expect(scope.industrySectors[2].list).toEqual([{name: "Basic Materials", sectorType:"BMC"}]);
+      expect(scope.industrySectors[2].selected).toEqual([{name: "Basic Materials", sectorType:"BMC"}]);
+
+      scope.removeFunction("Tech");
+      expect(scope.industrySectors[0].selected.length).toBe(0);
+      expect(scope.industrySectors[0].list).toEqual([{name: "Health", sectorType:"HC", selected: false}]);
+
+      expect(scope.industrySectors[1].selected.length).toBe(0);
+      expect(scope.industrySectors[1].list).toEqual([{name: "Tech", sectorType:"TC", selected: false}]);
+
+      expect(scope.industrySectors[2].selected.length).toBe(1);
+      expect(scope.industrySectors[2].list).toEqual([{name: "Basic Materials", sectorType:"BMC"}]);
+      expect(scope.industrySectors[2].selected).toEqual([{name: "Basic Materials", sectorType:"BMC"}]);
+
+      scope.removeFunction("Basic Materials");
+      expect(scope.industrySectors[0].selected.length).toBe(0);
+      expect(scope.industrySectors[0].list).toEqual([{name: "Health", sectorType:"HC", selected: false}]);
+
+      expect(scope.industrySectors[1].selected.length).toBe(0);
+      expect(scope.industrySectors[1].list).toEqual([{name: "Tech", sectorType:"TC", selected: false}]);
+
+      expect(scope.industrySectors[2].selected.length).toBe(0);
+      expect(scope.industrySectors[2].list).toEqual([{name: "Basic Materials", sectorType:"BMC", selected: false}]);
+
+      //moving elements in array
+      scope.industrySectors = [{name:"Healthcare Sector", selected:[{name: "Health", sectorType:"HC"},{name: "Health1", sectorType:"HC"},{name: "Health2", sectorType:"HC"}], list: [{name: "Health", sectorType:"HC"},{name: "Health1", sectorType:"HC"},{name: "Health2", sectorType:"HC"}]},{name:"Technology Sector", selected:[{name: "Tech", sectorType:"TC"},{name: "Tech1", sectorType:"TC"},{name: "Tech2", sectorType:"TC"}], list: [{name: "Tech", sectorType:"TC"},{name: "Tech1", sectorType:"TC"},{name: "Tech2", sectorType:"TC"}]},{name:"Basic Materials Sector", selected:[{name: "Basic Materials", sectorType:"BMC"},{name: "Basic1 Materials", sectorType:"BMC"},{name: "Basic2 Materials", sectorType:"BMC"}], list: [{name: "Basic Materials", sectorType:"BMC"},{name: "Basic1 Materials", sectorType:"BMC"},{name: "Basic2 Materials", sectorType:"BMC"}]}];
+      expect(scope.industrySectors[0].selected[0].name).toEqual("Health");
+      scope.removeFunction("Health");
+      expect(scope.industrySectors[0].selected[0].name).toEqual("Health2");
+      expect(scope.industrySectors[0].selected[1].name).toEqual("Health1");
+      expect(scope.industrySectors[0].selected.length).toBe(2);
+      scope.removeFunction("Health2");
+      expect(scope.industrySectors[0].selected[0].name).toEqual("Health1");
+      expect(scope.industrySectors[0].selected.length).toBe(1);
+
+      expect(scope.industrySectors[1].selected[2].name).toEqual("Tech2");
+      scope.removeFunction("Tech2");
+      expect(scope.industrySectors[1].selected[0].name).toEqual("Tech");
+      expect(scope.industrySectors[1].selected[1].name).toEqual("Tech1");
+      expect(scope.industrySectors[1].selected.length).toBe(2);
+      scope.removeFunction("Tech1");
+      expect(scope.industrySectors[1].selected[0].name).toEqual("Tech");
+      expect(scope.industrySectors[1].selected.length).toBe(1);
+
+      expect(scope.industrySectors[2].selected[1].name).toEqual("Basic1 Materials");
+      scope.removeFunction("Basic1 Materials");
+      expect(scope.industrySectors[2].selected[0].name).toEqual("Basic Materials");
+      expect(scope.industrySectors[2].selected[1].name).toEqual("Basic2 Materials");
+      expect(scope.industrySectors[2].selected.length).toBe(2);
+      scope.removeFunction("Basic2 Materials");
+      expect(scope.industrySectors[2].selected[0].name).toEqual("Basic Materials");
+      expect(scope.industrySectors[2].selected.length).toBe(1);
     }));
 
   });
@@ -52,7 +228,7 @@ describe('myApp.view4 module', function() {
       var returnData = { exist: true };
       httpBackend.expectGET('http://localhost:8080/app/Healthcare Sector.json').respond(returnData);
 
-      var returnedPromise = httpBasedService;
+      var returnedPromise = httpBasedService.get();
 
       var result;
       returnedPromise.then(function(response) {
@@ -81,7 +257,7 @@ describe('myApp.view4 module', function() {
       var returnData = { exist: true };
       httpBackend.expectGET('http://localhost:8080/app/Additional Sectors.json').respond(returnData);
 
-      var returnedPromise = secondHttpBasedService;
+      var returnedPromise = secondHttpBasedService.get();
 
       var result;
       returnedPromise.then(function(response) {
--- app/view4/view4.js
+++ app/view4/view4.js
@@ -31,30 +31,39 @@ angular.module('myApp.view4', ['ngRoute', 'ngAnimate'])
 })
 // service for HTTPRequest
 .factory('HealthCareSector', ['$http', function($http) {
-  return $http.get('http://localhost:8080/app/Healthcare Sector.json')
-    .success(function(data) {
-      return angular.fromJson(data);
-    })
-    .error(function(err) {
-      return err;
-    });
+  return {
+    get: function(){
+      return $http.get('http://localhost:8080/app/Healthcare Sector.json')
+        .success(function (data) {
+          return angular.fromJson(data);
+        })
+        .error(function (err) {
+          return err;
+        });
+    }
+  }
 }])
 
 .factory('additionalSectors', ['$http', function($http) {
-  return $http.get('http://localhost:8080/app/Additional Sectors.json')
-    .success(function(data) {
-      return angular.fromJson(data);
-    })
-    .error(function(err) {
-      return err;
-    });
+  return{
+    get: function(){
+      return $http.get('http://localhost:8080/app/Additional Sectors.json')
+        .success(function(data) {
+          return angular.fromJson(data);
+        })
+        .error(function(err) {
+          return err;
+        });
+    }
+  }
 }])
 //when user leaves from view4 - array with selected and list sectors is added to cache and got form cache when user returns to view4 from another view
 .controller('View4Ctrl', ['$scope','HealthCareSector','additionalSectors','$cacheFactory',function($scope,HealthCareSector,additionalSectors,$cacheFactory) {
   $scope.showDetails = true;
 //array with selected sectors and all sectors for each industry type
 //use array with selected sectors for display its names and total amount of selected sectors for each industry type
-  (function(){
+//anonymous function is not good enough in unit-testing
+  $scope.controllerFunction = function(){
     if(!angular.isUndefined($cacheFactory.get('industrySectors'))){
       $scope.industrySectors = $cacheFactory.get('industrySectors').get('industrySectors');//find cached value in cacheId
       $scope.selectedHC = true;
@@ -62,13 +71,14 @@ angular.module('myApp.view4', ['ngRoute', 'ngAnimate'])
     else{
       $scope.cache = $cacheFactory('industrySectors');//create cacheId and put there value if this cacheId not found
       $scope.industrySectors = [{name:"Healthcare Sector", selected:[], list: []},{name:"Technology Sector", selected:[], list: []},{name:"Basic Materials Sector", selected:[], list: []}];
-      HealthCareSector.success(function(data){
+      HealthCareSector.get().success(function(data){
         $scope.selectedHC = true;
         $scope.industrySectors[0].list = data.list;
       });
       $scope.cache.put('industrySectors', $scope.industrySectors)
     }
-  })();
+  };
+  $scope.controllerFunction()
 //add function which search by name for selected sector in array with all sectors and pushing selected into array with selected sectors
   $scope.addFunction = function(sectorName){
     function sectorSearch(element, index, array){
@@ -107,6 +117,14 @@ angular.module('myApp.view4', ['ngRoute', 'ngAnimate'])
         }
       }
     }
+    var innerRemoveFunction = function(n){
+      var removeItem = $scope.industrySectors[n].selected.filter(sectorSearch);
+      var indexOfRemoveItem = $scope.industrySectors[n].selected.indexOf(removeItem[0])
+      var lastItem = $scope.industrySectors[n].selected[$scope.industrySectors[n].selected.length-1];
+      $scope.industrySectors[n].selected[indexOfRemoveItem] = lastItem;
+      $scope.industrySectors[n].selected[$scope.industrySectors[n].selected.length-1] = removeItem[0];
+      $scope.industrySectors[n].selected.pop();
+    };
     var searchResult = (function(){
       if($scope.industrySectors[0].list.filter(sectorSearch)[0] !== undefined){return $scope.industrySectors[0].list.filter(sectorSearch)}
       else if($scope.industrySectors[1].list.filter(sectorSearch)[0] !== undefined){return $scope.industrySectors[1].list.filter(sectorSearch)}
@@ -114,28 +132,13 @@ angular.module('myApp.view4', ['ngRoute', 'ngAnimate'])
     })();
     //removing sector from appropriate industry type
     if(searchResult[0].sectorType === "HC"){
-      var removeItem = $scope.industrySectors[0].selected.filter(sectorSearch);
-      var indexOfRemoveItem = $scope.industrySectors[0].selected.indexOf(removeItem[0])
-      var lastItem = $scope.industrySectors[0].selected[$scope.industrySectors[0].selected.length-1];
-      $scope.industrySectors[0].selected[indexOfRemoveItem] = lastItem;
-      $scope.industrySectors[0].selected[$scope.industrySectors[0].selected.length-1] = removeItem[0];
-      $scope.industrySectors[0].selected.pop();
+      innerRemoveFunction(0);
     }
     else if(searchResult[0].sectorType === "TC"){
-      var removeItem = $scope.industrySectors[1].selected.filter(sectorSearch);
-      var indexOfRemoveItem = $scope.industrySectors[1].selected.indexOf(removeItem[0])
-      var lastItem = $scope.industrySectors[1].selected[$scope.industrySectors[1].selected.length-1];
-      $scope.industrySectors[1].selected[indexOfRemoveItem] = lastItem;
-      $scope.industrySectors[1].selected[$scope.industrySectors[1].selected.length-1] = removeItem[0];
-      $scope.industrySectors[1].selected.pop();
+      innerRemoveFunction(1);
     }
     else{
-      var removeItem = $scope.industrySectors[2].selected.filter(sectorSearch);
-      var indexOfRemoveItem = $scope.industrySectors[2].selected.indexOf(removeItem[0])
-      var lastItem = $scope.industrySectors[2].selected[$scope.industrySectors[2].selected.length-1];
-      $scope.industrySectors[2].selected[indexOfRemoveItem] = lastItem;
-      $scope.industrySectors[2].selected[$scope.industrySectors[2].selected.length-1] = removeItem[0];
-      $scope.industrySectors[2].selected.pop();
+      innerRemoveFunction(2);
     }
   };
   $scope.selectedProducts = function(selectedList){
@@ -148,38 +151,39 @@ angular.module('myApp.view4', ['ngRoute', 'ngAnimate'])
   };
   $scope.downloadSectors = function(){
     if($scope.industrySectors[1].list.length === 0 && $scope.industrySectors[2].list.length === 0){
-      additionalSectors.success(function(data){
+      additionalSectors.get().success(function(data){
         $scope.industrySectors[1].list = data.TechnologyList;
         $scope.industrySectors[2].list = data.BasicMaterialsList;
       });
     }
     else {
       if($scope.industrySectors[1].list.length === 0){
-        $scope.industrySectors[1].list = data.TechnologyList;
+        additionalSectors.get().success(function(data){
+          $scope.industrySectors[1].list = data.TechnologyList;
+        });
       }
       else if($scope.industrySectors[2].list.length === 0){
-        $scope.industrySectors[2].list = data.BasicMaterialsList;
+        additionalSectors.get().success(function(data){
+          $scope.industrySectors[2].list = data.BasicMaterialsList;
+        });
       }
     }
   };
   $scope.selectedSector = function(sectorName){
-    if(sectorName == $scope.industrySectors[0].name){
-      $scope.selectedHC = true;
-      $scope.selectedTC = false;
-      $scope.selectedBMC = false;
+    var innerSelectedSectorFunction = function(selectedHC,selectedTC,selectedBMC){
+      $scope.selectedHC = selectedHC;
+      $scope.selectedTC = selectedTC;
+      $scope.selectedBMC = selectedBMC;
       $scope.downloadSectors()
+    }
+    if(sectorName == $scope.industrySectors[0].name){
+      innerSelectedSectorFunction(true,false,false);
     }
     else if (sectorName == $scope.industrySectors[1].name){
-      $scope.selectedHC = false;
-      $scope.selectedTC = true;
-      $scope.selectedBMC = false;
-      $scope.downloadSectors()
+      innerSelectedSectorFunction(false,true,false);
     }
     else{
-      $scope.selectedHC = false;
-      $scope.selectedTC = false;
-      $scope.selectedBMC = true;
-      $scope.downloadSectors()
+      innerSelectedSectorFunction(false,false,true);
     }
   }
 }])
